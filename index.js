f8151f1655aeef9a72245b36a5ee33327432cf60
Bryan Lecza <brylex0324@gmail.com>
2023-01-13 13:56:11 -0500

    Dice and Banns

--- index.js
+++ index.js
@@ -12,24 +12,44 @@ const client = new Client({
     ]
 })
 
+const banned_users = []
+
+
 client.on('ready', (c) => {
     console.log(`${c.user.username} is online.`)
 })
 
 client.on('messageCreate', (message) => {
     if (message.content.toLowerCase() === "hi bry bot") {
-        message.reply(`hello ${message.author.username}`)
-    }
-    
-    else if(message.content.toLowerCase() === "flip a coin") {
-        const state = Math.floor(Math.random() * 2)
-        if (state === 1){
-            message.reply('Heads')
+        if (banned_users.includes(message.author.username)) {
+            message.reply('screw off')
         }
         else {
-            message.reply('Tails')
+            message.reply(`hello ${message.author.username}`)
         }
     }
+
+    else if(message.content.toLowerCase() === "flip a coin") {
+        if (banned_users.includes(message.author.username)) {
+            message.reply('screw off')
+        }
+
+        else {
+            const state = Math.floor(Math.random() * 2)
+            if (state === 1){
+                message.reply('Heads')
+            }
+            else {
+                message.reply('Tails')
+            }
+        }
+    }
+
+    else if(message.content.toLowerCase() === 'roll a die') {
+        const num = Math.floor(Math.random() * (7 - 1) + 1)
+        message.reply(`${num}`)
+    }
+    
     
 
 })
